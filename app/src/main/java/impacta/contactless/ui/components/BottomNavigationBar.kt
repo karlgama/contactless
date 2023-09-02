39874bf0ce04d5c2f04da905029916df3c5d778c
kaique <kaiquesilva49@hotmail.com>
2023-09-02 15:36:18 -0300

    separating bottom navigation item

--- app/src/main/java/impacta/contactless/ui/components/BottomNavigationBar.kt
+++ app/src/main/java/impacta/contactless/ui/components/BottomNavigationBar.kt
@@ -1,24 +1,20 @@
 package impacta.contactless.ui.components
 
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.material.BottomNavigation
 import androidx.compose.material.BottomNavigationItem
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material.icons.filled.AccountCircle
-import androidx.compose.material.icons.filled.Favorite
-import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Home
 import androidx.compose.material.icons.filled.Settings
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.tooling.preview.Preview
 import impacta.contactless.ui.theme.LightGreen
 
 @Composable
@@ -27,30 +23,27 @@ fun BottomNavigationBar() {
         backgroundColor = Color.White,
         modifier = Modifier.fillMaxWidth()
     ) {
-        BottomNavigationItem(
-            icon = { Icon(Icons.Default.Home,"") },
-            label = { Text("Chaves") },
-            selected = true,
-            onClick ={}
-        )
-        BottomNavigationItem(
-            icon = { Icon(Icons.Default.AccountCircle,"") },
-            label = { Text("Perfil") },
-            selected = true,
-            onClick = {}
-        )
-        BottomNavigationItem(
-            icon = { Icon(Icons.Default.Settings,"") },
-            label = { Text("Configurações") },
-            selected = true,
-            onClick = {}
-        )
+        navigationItem("Chaves", Icons.Default.Home,true)
+        navigationItem("Perfil", Icons.Default.AccountCircle)
+        navigationItem("Configurações", Icons.Default.Settings)
     }
 }
 
+@Composable
+private fun RowScope.navigationItem(text: String, icon: ImageVector, selected: Boolean = false) {
+    BottomNavigationItem(
+        icon = { Icon(icon, "") },
+        label = { Text(text) },
+        selected = selected,
+        selectedContentColor = LightGreen,
+        unselectedContentColor = Color.Black,
+        onClick = {}
+    )
+}
 
 @Preview
 @Composable
 fun preview() {
     BottomNavigationBar()
 }
+
