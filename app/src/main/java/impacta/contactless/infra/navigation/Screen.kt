4c646565c3327f788103f3f8d9a7f2df349d70b7
Saulo Freire <saulo.freire@petzpartner.com.br>
2023-11-03 19:31:57 -0300

    feat(settings): change settings to profile screen

--- app/src/main/java/impacta/contactless/infra/navigation/Screen.kt
+++ app/src/main/java/impacta/contactless/infra/navigation/Screen.kt
@@ -3,12 +3,13 @@ package impacta.contactless.infra.navigation
 import androidx.annotation.StringRes
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.ui.graphics.vector.ImageVector
 import impacta.contactless.R
 
 sealed class Screen(val route: String, @StringRes val resourceId: Int, val icon: ImageVector) {
     object ActiveKeys : Screen("keys", R.string.app_name, Icons.Filled.Home)
-    object Settings: Screen("settings", R.string.settings_screen, Icons.Filled.Settings)
+    object Settings: Screen("settings", R.string.settings_screen, Icons.Default.Person)
     object SignIn: Screen("sign_in", R.string.sign_in, Icons.Filled.Settings)
 }
