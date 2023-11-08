a2dc6ba21325c7147b86a53b59e865a5ca929c99
Saulo Freire <saulo.freire@petzpartner.com.br>
2023-11-08 20:44:51 -0300

    fix(Interceptor): base url replacement

--- app/src/main/java/impacta/contactless/infra/network/BaseURLInterceptor.kt
+++ app/src/main/java/impacta/contactless/infra/network/BaseURLInterceptor.kt
@@ -1,5 +1,6 @@
 package impacta.contactless.infra.network
 
+import android.util.Log
 import android.util.Patterns
 import impacta.contactless.infra.Constants
 import okhttp3.Interceptor
@@ -9,11 +10,12 @@ class BaseURLInterceptor: Interceptor {
     override fun intercept(chain: Interceptor.Chain): Response {
         var originalRequest = chain.request()
         val requestURL = originalRequest.url.toString()
-        val protocol = "(?i:http|https|rtsp)://"
-        var newURL = requestURL.replaceFirst(protocol, "")
-            .replaceFirst(Patterns.DOMAIN_NAME.toString(), "")
+        val protocol = "(?i:http|https|rtsp)://gm"
+        var newURL = (requestURL
+            .replaceBefore("v1", ""))
         newURL = if (validateBackSlash(newURL)) Constants.kBaseURL.plus(newURL) else newURL.replaceFirst("/", Constants.kBaseURL)
         originalRequest = originalRequest.newBuilder().url(newURL).build()
+        Log.d("BaseURLInterceptor", newURL)
         return chain.proceed(originalRequest)
     }
 
